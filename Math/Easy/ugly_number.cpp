3a0f9a6e96dc065736da8ba02c61247bb0069309
Ibraheem-cpp <ibraheemtahir84@gmail.com>
2026-06-03 23:29:12 +0500

    Ugly Number
    An ugly number is a positive integer which does not have a prime factor other than 2, 3, and 5.
    Given an integer n, return true if n is an ugly number.

---
+++ Math/Easy/ugly_number.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isUgly(int n) {
+        if(n <= 0) return false;
+        
+        while(n % 2 == 0){
+            n = n/2;
+        }
+        while(n % 3 == 0){
+            n = n/3;
+        }
+        while(n % 5 == 0){
+            n = n/5;
+        }
+
+        if(n == 1) return true;
+        return false;
+
+        //      Time Complexity -> O(log n)
+        //      Space Complexity -> O(1)
+}
+
+int main(){
+
+    if(isUgly(25)){
+        cout << "Yes.\n";
+    }
+    else{
+        cout << "No.\n";
+    }
+
+    return 0;
+}
