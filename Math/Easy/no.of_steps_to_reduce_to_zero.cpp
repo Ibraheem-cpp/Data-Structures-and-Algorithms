a980e3dc66963f87d2c5b3b6c2094059256e4dbe
Ibraheem-cpp <ibraheemtahir84@gmail.com>
2026-07-09 23:33:04 +0500

    Number of Steps to Reduce to Zero
    Given an integer num, return the number of steps to reduce it to zero.
    
    In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.

---
+++ Math/Easy/no.of_steps_to_reduce_to_zero.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int numberOfSteps(int num) {
+        int steps = 0;
+        while(num >= 1){
+            if(num % 2 == 0) num /= 2;
+            else num -= 1;
+
+            steps++;
+        }
+
+        return steps;
+
+        //      Time Complexity -> O(n)
+        //      Space Complexity -> O(1)
+}
+
+int main(){
+
+    /*
+        Constraints:
+            0 <= num <= 10^6
+    */
+
+    int num = 3526;
+    cout << numberOfSteps(num) << endl;
+
+    return 0;
+}
